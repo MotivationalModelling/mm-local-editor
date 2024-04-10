524bb92df31fbb21f7cad78b7a2903f9d8dd43e7
frankwan41 <80295318+frankwan41@users.noreply.github.com>
2024-04-10 11:03:35 +1000

    Finish implementing resize panels

--- src/components/ProjectEdit.tsx
+++ src/components/ProjectEdit.tsx
@@ -5,13 +5,21 @@ import { useState } from "react";
 
 const ProjectEdit = () => {
   const [showGoalSection, setShowGoalSection] = useState(false);
+  const [showGraphSection, setShowGraphSection] = useState(false);
 
   return (
     <div>
       <ProjectEditHeader />
-      <SectionPanel showGoalSection={showGoalSection} />
+      <SectionPanel
+        showGoalSection={showGoalSection}
+        showGraphSection={showGraphSection}
+        paddingX={15}
+      />
       <button onClick={() => setShowGoalSection(!showGoalSection)}>
         Show Section 1
+      </button>
+      <button onClick={() => setShowGraphSection(!showGraphSection)}>
+        Show Section 3
       </button>
     </div>
   );
--- src/components/SectionPanel.tsx
+++ src/components/SectionPanel.tsx
@@ -1,4 +1,5 @@
-import { Resizable } from "re-resizable";
+import { useEffect, useRef, useState } from "react";
+import { Resizable, ResizeCallback } from "re-resizable";
 import "./SectionPanel.css";
 
 const defaultStyle = {
@@ -8,58 +9,134 @@ const defaultStyle = {
   border: "solid 1px #ddd",
 };
 
-type SectionPanelProps = {
-  showGoalSection: boolean;
+const DEFINED_PROPOTIONS = {
+  maxWidth: "80%",
+  minWidth: "10%",
 };
 
-const SectionPanel = ({ showGoalSection }: SectionPanelProps) => {
+type SectionPanelProps = {
+  showGoalSection: boolean;
+  showGraphSection: boolean;
+  paddingX: number;
+};
+
+const SectionPanel = ({
+  showGoalSection,
+  showGraphSection,
+  paddingX,
+}: SectionPanelProps) => {
+  const [sectionOneWidth, setSectionOneWidth] = useState(0);
+  const [sectionThreeWidth, setSectionThreeWidth] = useState(0);
+  const [parentWidth, setParentWidth] = useState(0);
+
+  const sectionTwoRef = useRef<HTMLDivElement>(null);
+  const parentRef = useRef<HTMLDivElement>(null);
+
+  // Handle section one resize and section three auto resize
+  const handleResizeSectionOne: ResizeCallback = (
+    event,
+    direction,
+    ref,
+    delta
+  ) => {
+    setSectionOneWidth(ref.offsetWidth);
+    // If the width sum exceed the parent total width, auto resize the section three until reach the minimum
+    if (
+      sectionTwoRef.current &&
+      ref.offsetWidth + sectionTwoRef.current.offsetWidth + sectionThreeWidth >=
+        parentWidth
+    ) {
+      setSectionThreeWidth(
+        parentWidth - ref.offsetWidth - sectionTwoRef.current.offsetWidth
+      );
+    }
+  };
+
+  // Handle section three resize and section one auto resize
+  const handleResizeSectionThree: ResizeCallback = (
+    event,
+    direction,
+    ref,
+    delta
+  ) => {
+    setSectionThreeWidth(ref.offsetWidth);
+    // If the width sum exceed the parent total width, auto resize the section one until reach the minimum
+    if (
+      sectionTwoRef.current &&
+      sectionOneWidth + sectionTwoRef.current.offsetWidth + ref.offsetWidth >=
+        parentWidth
+    ) {
+      setSectionOneWidth(
+        parentWidth - ref.offsetWidth - sectionTwoRef.current.offsetWidth
+      );
+    }
+  };
+
+  // Get the parent div inner width and set initial width for section three
+  useEffect(() => {
+    if (parentRef.current) {
+      setParentWidth(parentRef.current?.clientWidth - paddingX * 2);
+      setSectionThreeWidth(parentWidth * 0.5);
+    }
+  }, []);
+
   return (
     <div
       style={{
-        // width: "100%",
+        width: "100%",
         display: "flex",
-        padding: "15px",
+        padding: paddingX,
       }}
+      ref={parentRef}
     >
+      {/* Goal List Section */}
       <Resizable
-        // bounds="parent"
         handleClasses={{ right: "right-handler" }}
         enable={{ right: true }}
         style={{
           ...defaultStyle,
           backgroundColor: "rgb(236, 244, 244)",
-          display: showGoalSection ? "" : "none",
+          display: showGoalSection ? "flex" : "none",
         }}
-        defaultSize={{ width: "30%", height: "200px" }}
-        maxWidth="80%"
-        minWidth="10%"
+        size={{ width: sectionOneWidth, height: "200px" }}
+        maxWidth={DEFINED_PROPOTIONS.maxWidth}
+        minWidth={DEFINED_PROPOTIONS.minWidth}
+        onResize={handleResizeSectionOne}
       >
         {/* First Panel Content */}
         Section 1
       </Resizable>
 
+      {/* Cluster Hierachy Section */}
       <div
         style={{
           ...defaultStyle,
           width: "100%",
-          // display: "none",
+          minWidth: DEFINED_PROPOTIONS.minWidth,
+          height: 200,
           backgroundColor: "rgba(35, 144, 231, 0.1)",
         }}
+        ref={sectionTwoRef}
       >
         Section 2
       </div>
+
+      {/* Graph Render Section */}
       <Resizable
-        // bounds="parent"
         handleClasses={{ left: "left-handler" }}
         enable={{ left: true }}
         style={{
           ...defaultStyle,
           backgroundColor: "rgb(236, 244, 244)",
-          // display: "none",
+          display: showGraphSection ? "flex" : "none",
         }}
-        defaultSize={{ width: "30%", height: "200px" }}
-        maxWidth="80%"
-        minWidth="10%"
+        size={{
+          width: sectionThreeWidth,
+          height: 200,
+        }}
+        maxWidth={DEFINED_PROPOTIONS.maxWidth}
+        minWidth={DEFINED_PROPOTIONS.minWidth}
+        onResize={handleResizeSectionThree}
       >
         {/* Third Panel Content */}
         Section 3
