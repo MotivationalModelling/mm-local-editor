8e00e1f3bd320b0aff8b1ab2afee1c9153d0ccdd
Ben Golding <bgg@object-craft.com.au>
2024-04-18 15:47:48 +1000

    Formatting. Use React.FC to flag component.

--- src/components/ProjectEdit.tsx
+++ src/components/ProjectEdit.tsx
@@ -1,26 +1,23 @@
+import React, {useState} from "react";
+
 import ProjectEditHeader from "./ProjectEditHeader";
 import "./ProjectEdit.css";
 import SectionPanel from "./SectionPanel";
-import { useState } from "react";
 import ProgressBar from "./ProgressBar";
 
-const ProjectEdit = () => {
-  const [showGoalSection, setShowGoalSection] = useState<boolean>(true);
-  const [showGraphSection, setShowGraphSection] = useState<boolean>(false);
+const ProjectEdit: React.FC = () => {
+  const [showGoalSection, setShowGoalSection] = useState(true);
+  const [showGraphSection, setShowGraphSection] = useState(false);
 
   return (
     <div>
       <ProjectEditHeader />
-      <ProgressBar
-        setShowGoalSection={setShowGoalSection}
-        setShowGraphSection={setShowGraphSection}
-      />
-      <SectionPanel
-        showGoalSection={showGoalSection}
-        showGraphSection={showGraphSection}
-        setShowGoalSection={setShowGoalSection}
-        paddingX={15}
-      />
+        <ProgressBar setShowGoalSection={setShowGoalSection}
+                     setShowGraphSection={setShowGraphSection}/>
+        <SectionPanel showGoalSection={showGoalSection}
+                      showGraphSection={showGraphSection}
+                      setShowGoalSection={setShowGoalSection}
+                      paddingX={15}/>
       {/* <button onClick={() => setShowGoalSection(!showGoalSection)}>
         Show Section 1
       </button> */}
