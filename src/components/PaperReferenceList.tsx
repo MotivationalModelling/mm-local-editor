3cd4bf2cacf813b6e9e93833c06eb29edee44081
Ben Golding <bgg@object-craft.com.au>
2024-04-18 16:10:39 +1000

    Improve the description of papers and add in a summary. Display papers as a list of Cards.  Still needs work.

---
+++ src/components/PaperReferenceList.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+
+import {Link} from "react-router-dom";
+
+import Card from "react-bootstrap/Card";
+import ListGroup from "react-bootstrap/ListGroup";
+
+export interface Reference {
+    title: string
+    link: string
+    summary?: string
+}
+
+const PaperReference: React.FC<{paper: Reference}> = ({paper}) => {
+    return (
+        <Card>
+            <Card.Header as="h5">
+                {paper.title}
+            </Card.Header>
+            <Card.Body>
+                {(paper.summary) && (
+                    <Card.Text>
+                        {paper.summary}
+                    </Card.Text>
+                )}
+            </Card.Body>
+            <Card.Footer>
+                <Link to={paper.link} target="_blank">
+                    Download
+                </Link>
+            </Card.Footer>
+        </Card>
+    );
+};
+
+interface Props {
+    references: Reference[]
+}
+
+const PaperReferenceList: React.FC<Props> = ({references}) => {
+    return (
+        <ListGroup as="ul" variant="flush">
+            {references.map((paper, i) => (
+                <ListGroup.Item as="li" key={i}>
+                    <PaperReference paper={paper} key={i}/>
+                </ListGroup.Item>
+            ))}
+        </ListGroup>
+    );
+};
+
+export default PaperReferenceList;
