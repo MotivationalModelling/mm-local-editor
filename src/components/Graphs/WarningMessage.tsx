1073ebf47ca19c33267ae6c76cc1348d5140ef20
Ben Golding <bgg@object-craft.com.au>
2024-10-01 16:26:35 +1000

    Formatting.

--- src/components/Graphs/WarningMessage.tsx
+++ src/components/Graphs/WarningMessage.tsx
@@ -8,13 +8,13 @@ const WarningMessage: React.FC<Props> = ({message}) => {
   return (
       <span className="text-warning"
             style={{
-          // position: "fixed",
-          bottom: "10px",
-          right: "10px",
-          padding: "10px",
-          borderRadius: "5px",
-          zIndex: 5,
-      }}>
+                // position: "fixed",
+                bottom: "10px",
+                right: "10px",
+                padding: "10px",
+                borderRadius: "5px",
+                zIndex: 5,
+            }}>
       {message}
     </span>
   );
