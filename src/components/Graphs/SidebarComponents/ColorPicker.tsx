deeefe9c97e154ef72c38d257065f1e3544097a4
Ben Golding <bgg@object-craft.com.au>
2025-03-25 15:31:01 +1100

    Formatting.

--- src/components/Graphs/SidebarComponents/ColorPicker.tsx
+++ src/components/Graphs/SidebarComponents/ColorPicker.tsx
@@ -1,28 +1,29 @@
-import { useState } from "react";
-import { ColorResult, CompactPicker } from "react-color";
+import {useState} from "react";
+import {ColorResult, CompactPicker} from "react-color";
 
 type ColorPickerProps = {
-  selectedColor: string;
-  onColorChange: (color: ColorResult) => void;
+    selectedColor: string;
+    onColorChange: (color: ColorResult) => void;
 };
 
-const ColorPicker = ({ selectedColor, onColorChange }: ColorPickerProps) => {
-  const [showPicker, setShowPicker] = useState<boolean>(false);
+const ColorPicker = ({selectedColor, onColorChange}: ColorPickerProps) => {
+    const [showPicker, setShowPicker] = useState<boolean>(false);
 
-  const handleButtonClick = () => {
-    setShowPicker(!showPicker);
-  };
+    const handleButtonClick = () => {
+        setShowPicker(!showPicker);
+    };
 
-  return (
-    <div>
-      {showPicker && (
-        <div style={{ position: "absolute", top: "10px", right: "250px" }}>
-          <CompactPicker color={selectedColor} onChangeComplete={onColorChange}/>
+    return (
+        <div>
+            {(showPicker) && (
+                <div style={{position: "absolute", top: "10px", right: "250px"}}>
+                    <CompactPicker color={selectedColor}
+                                   onChangeComplete={onColorChange}/>
+                </div>
+            )}
+            <button onClick={handleButtonClick}>Select Colour</button>
         </div>
-      )}
-      <button onClick={handleButtonClick}>Select Colour</button>
-    </div>
-  );
+    );
 };
 
 export default ColorPicker;
