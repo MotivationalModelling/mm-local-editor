8f54e9465116fa17ce4e5b218e055440a47f156b
Ben Golding <bgg@object-craft.com.au>
2025-08-25 22:07:09 +1000

    Refactor and reorder the icons in the SideBarBody. Make border rounded.
    #46

--- src/components/Graphs/SidebarComponents/SidebarBody.tsx
+++ src/components/Graphs/SidebarComponents/SidebarBody.tsx
@@ -1,34 +1,30 @@
 import {Graph} from "@maxgraph/core";
 import ColorButtons from "./ColorButtons";
 import ZoomButtons from "./ZoomButtons";
-import SidebarItems from "./SidebarItems";
 import ScaleTextButton from "../ScaleTextButton";
 import {CollapsibleSidebarCard} from "./CollapsibleSidebarCard";
 
-type SidebarProps = {
-  graph: Graph | null;
-  recentreView: () => void;
-};
+interface SidebarProps {
+    graph: Graph | null
+    recentreView: () => void
+}
 
-const SidebarBody = ({ graph, recentreView }: SidebarProps) => {
-  if (!graph) return null;
+const SidebarBody = ({graph, recentreView}: SidebarProps) => {
+    if (!graph) return null;
 
-  return (
-    // <div id="sidebarContainer">
-    <div>
-      <CollapsibleSidebarCard title="Colour">
-        <ColorButtons graph={graph} />
-      </CollapsibleSidebarCard>
-      <CollapsibleSidebarCard title="Font size">
-        <ScaleTextButton/>
-      </CollapsibleSidebarCard>
-      <CollapsibleSidebarCard isOpen title="Zoom">
-        <ZoomButtons graph={graph}
-                     recentreView={recentreView} />
-      </CollapsibleSidebarCard>
-        <SidebarItems graph={graph} />
-    </div>
-  );
+    return (
+        <div className="border border-black p-1 rounded">
+            <CollapsibleSidebarCard isOpen title="Zoom">
+                <ZoomButtons recentreView={recentreView}/>
+            </CollapsibleSidebarCard>
+            <CollapsibleSidebarCard title="Colour">
+                <ColorButtons graph={graph}/>
+            </CollapsibleSidebarCard>
+            <CollapsibleSidebarCard title="Font size">
+                <ScaleTextButton/>
+            </CollapsibleSidebarCard>
+        </div>
+    );
 };
 
 export default SidebarBody;
