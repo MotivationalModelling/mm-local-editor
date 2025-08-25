3cf6fe72e32803c3eea65f6d113f0e57384546c2
Ben Golding <bgg@object-craft.com.au>
2025-08-25 22:13:41 +1000

    Move ColourPicker into the SidebarBody. Only render SidebarItems when there is a graph.
    #46

--- src/components/Graphs/GraphSidebar.tsx
+++ src/components/Graphs/GraphSidebar.tsx
@@ -3,6 +3,7 @@ import { ColorResult } from "react-color";
 import { Graph } from "@maxgraph/core";
 import ColorPicker from "./SidebarComponents/ColorPicker";
 import SidebarBody from "./SidebarComponents/SidebarBody";
+import SidebarItems from "./SidebarComponents/SidebarItems.tsx";
 
 type recentreViewFunction = () => void;
 
@@ -12,30 +13,12 @@ type GraphSidebarProps = {
 };
 
 const GraphSidebar = ({graph, recentreView}: GraphSidebarProps) => {
-    const [selectedColor, setSelectedColor] = useState<string>("#ffffff");
-
-    // Handler for changing colours
-    const handleColorChange = (color: ColorResult) => {
-        setSelectedColor(color.hex);
-        if (graph) {
-            graph.getDataModel().beginUpdate();
-            try {
-                for (const cell of graph.getSelectionCells()) {
-                    const style = graph.getCellStyle(cell);
-
-                    style.fillColor = color.hex;
-                    graph.getDataModel().setStyle(cell, style);
-                }
-            } finally {
-                graph.getDataModel().endUpdate();
-            }
-        }
-    };
-
     return (
         <div>
-            <ColorPicker selectedColor={selectedColor} onColorChange={handleColorChange}/>
             <SidebarBody graph={graph} recentreView={recentreView}/>
+            {(graph) && (
+                <SidebarItems graph={graph}/>
+            )}
         </div>
     );
 };
