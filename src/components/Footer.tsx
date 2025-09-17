ec3e81159089159a648da2fed36728431ffb37b2
Rin <lvqq2000@gmail.com>
2025-09-17 10:49:18 +0800

    feat(footer): show version number in the footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,13 @@
 // Copyrights footer
+
+import pkg from '../../package.json';
+
 const Footer = () => {
   return (
     <div className="mt-auto">
       <p className="text-center">
+        Version: {pkg.version}
+        <br />
         &copy;<em>2018-2025 Queue Solutions Pty Ltd</em>
         <br />
         All rights reserved
