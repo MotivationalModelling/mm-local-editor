57bb56f6686a7432c91136eff7f9938376f1a98e
Ben Golding <bgg@object-craft.com.au>
2025-09-19 17:23:46 +1000

    Remove ShowGoalListButton.
    #129

--- src/components/header/ProjectEditHeader.tsx
+++ src/components/header/ProjectEditHeader.tsx
@@ -9,7 +9,6 @@ import {isChrome, isEdge, isOpera} from "react-device-detect";
 import {reset} from "../context/treeDataSlice.ts";
 import {initialTabs} from "../../data/initialTabs.ts";
 import ResetGraphButton from "../Graphs/ResetGraphButton.tsx";
-import ShowGoalSectionButton from "./ShowGoalSectionButton.tsx";
 
 type ProjectEditHeaderProps = {
 	showGoalSection: boolean;
@@ -47,8 +46,6 @@ const ProjectEditHeader: React.FC<ProjectEditHeaderProps> = ({
                         <strong style={{fontSize: "35px"}}>AMMBER</strong>
                     </Col>
                     <Col className="d-flex flex-column flex-sm-row gap-2 justify-content-end align-items-center">
-                        <ShowGoalSectionButton onClick={() => setShowGoalSection(!showGoalSection)}
-                                               showGoalSection={showGoalSection}/>
                         <ResetGraphButton variant="outline-primary" className="ms-3"/>
                         <ButtonGroup className="ms-3">
                             {/* Pass showGraphSection to ExportFileButton to control enablement */}
@@ -59,18 +56,6 @@ const ProjectEditHeader: React.FC<ProjectEditHeaderProps> = ({
                             Back
                         </Button>
                     </Col>
-                    {/* <Col className="text-end align-content-center" xs={8}>
-						<ButtonGroup>
-							<ExportFileButton />
-							{isBrowserSupported && <SaveFileButton />}
-						</ButtonGroup>
-						
-					</Col>		 */}
-                    {/* <Col className="text-end align-content-center">
-						<Button variant="primary" onClick={handleBackBtnClick}>
-							Back
-						</Button>
-					</Col> */}
                 </Row>
             </Container>
         </header>
