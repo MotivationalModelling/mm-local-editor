391af227647267d51882f3f0beafd1a783f8cc53
Rin <lvqq2000@gmail.com>
2026-01-05 16:17:53 +1100

    chore: small change to indent and component

--- src/components/ProgressBar.tsx
+++ src/components/ProgressBar.tsx
@@ -4,11 +4,11 @@ import {OverlayTrigger, Popover} from "react-bootstrap";
 import {GraphUsageInfo} from "./UsageInfo/GraphUsageInfo";
 import {ClusterUsageInfo} from "./UsageInfo/ClusterUsageInfo";
 import ShowGoalSectionButton from "./header/ShowGoalSectionButton";
-import {OverlayInfoIcon} from "./UsageInfo/OverlayInfoIcon";
+import {BsInfoCircleFill} from "react-icons/bs";
 
 enum TabOptions {
-  Cluster,
-  Graph,
+    Cluster,
+    Graph,
 }
 
 interface ProgressBarProps {
@@ -18,86 +18,92 @@ interface ProgressBarProps {
 }
 
 const ProgressBar = ({
-                         showGoalSection,
-                         setShowGoalSection,
-                         setShowGraphSection,
-                     }: ProgressBarProps) => {
-  const [selectedTab, setSelectedTab] = useState(TabOptions.Cluster);
+                        showGoalSection,
+                        setShowGoalSection,
+                        setShowGraphSection,
+                    }: ProgressBarProps) => {
+    const [selectedTab, setSelectedTab] = useState(TabOptions.Cluster);
 
-  const handleClusterBarClick = () => {
-    setSelectedTab(TabOptions.Cluster);
-    setShowGoalSection(true);
-    setShowGraphSection(false);
-  };
+    const handleClusterBarClick = () => {
+        setSelectedTab(TabOptions.Cluster);
+        setShowGoalSection(true);
+        setShowGraphSection(false);
+    };
 
-  const handleGraphBarClick = () => {
-    setSelectedTab(TabOptions.Graph);
-    setShowGoalSection(false);
-    setShowGraphSection(true);
-  };
+    const handleGraphBarClick = () => {
+        setSelectedTab(TabOptions.Graph);
+        setShowGoalSection(false);
+        setShowGraphSection(true);
+    };
 
-  return (
-      <div style={{
-          width: "auto",
-          minWidth: "1280px",
-          overflowY: "hidden",
-          maxWidth: "100%",
-      }}>
-      <div className="arrow-steps clearfix mb-1">
-          <div className={`step ${(selectedTab === TabOptions.Cluster) ? "current" : ""}`}
-               id="clusterTab"
-               onClick={handleClusterBarClick}>
-              <span>
-                Enter Goals / Arrange Hierarchy
-                <OverlayTrigger
-                  trigger="click"
-                  placement="left"
-                  rootClose
-                  overlay={
-                    <Popover>
-                      <Popover.Body>
-                        <ClusterUsageInfo />
-                      </Popover.Body>
-                    </Popover>
-                  }
-                >
-                  <OverlayInfoIcon />
-                </OverlayTrigger>
-              </span>
-          </div>
-          <div className={`step ${(selectedTab === TabOptions.Graph) ? "current" : ""}`}
-               id="graphTab"
-               onClick={handleGraphBarClick}>
-          <span>
-              Arrange Hierarchy / Render Model
-              <OverlayTrigger
-                trigger="click"
-                placement="left"
-                rootClose
-                overlay={
-                  <Popover>
-                    <Popover.Body>
-                      <GraphUsageInfo />
-                    </Popover.Body>
-                  </Popover>
-                }
-              >
-                <OverlayInfoIcon />
-              </OverlayTrigger>
-              <ShowGoalSectionButton showGoalSection={showGoalSection}
-                                     onClick={(ev) => {
-                                         setShowGoalSection(!showGoalSection);
-                                         ev.stopPropagation();
-                                     }}
-                                     // make button stand out on dark background colour
-                                     variant={(selectedTab === TabOptions.Graph) ? "info" : undefined}
-                                     size="xs"
-                                     className="ms-1"/>
-          </span>
+    return (
+        <div style={{
+            width: "auto",
+            minWidth: "1280px",
+            overflowY: "hidden",
+            maxWidth: "100%",
+        }}>
+            <div className="arrow-steps clearfix mb-1">
+                <div className={`step ${(selectedTab === TabOptions.Cluster) ? "current" : ""}`}
+                    id="clusterTab"
+                    onClick={handleClusterBarClick}>
+                    <span>
+                        Enter Goals / Arrange Hierarchy
+                        <OverlayTrigger
+                            trigger="click"
+                            placement="left"
+                            rootClose
+                            overlay={
+                                <Popover>
+                                <Popover.Body>
+                                    <ClusterUsageInfo />
+                                </Popover.Body>
+                                </Popover>
+                            }
+                        >
+                            {/* Wrap with <span> because OverlayTrigger needs to attach a ref to its child to make it visible;
+                            react-icons components do not forward refs. */}
+                            <span>
+                                <BsInfoCircleFill className="ms-1"/>
+                            </span>
+                        </OverlayTrigger>
+                    </span>
+                </div>
+                <div className={`step ${(selectedTab === TabOptions.Graph) ? "current" : ""}`}
+                    id="graphTab"
+                    onClick={handleGraphBarClick}>
+                    <span>
+                        Arrange Hierarchy / Render Model
+                        <OverlayTrigger
+                            trigger="click"
+                            placement="left"
+                            rootClose
+                            overlay={
+                                <Popover>
+                                    <Popover.Body>
+                                    <GraphUsageInfo />
+                                    </Popover.Body>
+                                </Popover>
+                            }
+                        >
+                            <span>
+                                <BsInfoCircleFill className="ms-1"/>
+                            </span>
+                        </OverlayTrigger>
+                        <ShowGoalSectionButton showGoalSection={showGoalSection}
+                                                onClick={(ev) => {
+                                                    setShowGoalSection(!showGoalSection);
+                                                    ev.stopPropagation();
+                                                }}
+                                                // make button stand out on dark background colour
+                                                variant={(selectedTab === TabOptions.Graph) ? "info" : undefined}
+                                                size="xs"
+                                                className="ms-1"/>
+                    </span>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default ProgressBar;
