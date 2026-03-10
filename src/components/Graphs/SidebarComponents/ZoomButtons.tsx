afe29e8653b1b2b26f21f677065b9a8594b9080d
KerrYuan <yuaxy3@student.unimelb.edu.au>
2026-03-10 21:22:09 +0800

    fix: switches between flex-column and flex-row based on the container's width

--- src/components/Graphs/SidebarComponents/ZoomButtons.tsx
+++ src/components/Graphs/SidebarComponents/ZoomButtons.tsx
@@ -2,6 +2,7 @@ import ButtonGroup from "react-bootstrap/ButtonGroup";
 import Button from "react-bootstrap/Button";
 import {BsStopCircle, BsZoomIn, BsZoomOut} from "react-icons/bs";
 import {useGraph} from "../../context/GraphContext.tsx";
+import { useEffect, useRef, useState } from "react";
 
 type ZoomButtonsProps = {
   recentreView: () => void
@@ -9,9 +10,29 @@ type ZoomButtonsProps = {
 
 const ZoomButtons = ({recentreView}: ZoomButtonsProps) => {
     const {graph} = useGraph();
+    const HORIZONTAL_THRESHOLD = 90; 
+    const containerRef = useRef<HTMLDivElement>(null);
+    const [containerWidth, setContainerWidth] = useState(0);
+  
+    useEffect(() => {
+        const container = containerRef.current;
+
+        if (container) {
+            const resizeObserver = new ResizeObserver(() => {
+                setContainerWidth(container.offsetWidth);
+            });
+            resizeObserver.observe(container);
+
+            return () => {
+                resizeObserver.disconnect();
+            };
+        }
+    }, []);
 
     return (
-        <ButtonGroup className="w-100 d-flex flex-wrap" size="sm">
+        <ButtonGroup ref={containerRef} 
+                    className={`w-100 d-flex ${containerWidth < HORIZONTAL_THRESHOLD ? "flex-column" : "flex-row"}`} 
+                    size="sm">
             <Button className="flex-fill"
                     variant="light"
                     size="sm"
