078f742241d904bd66b13d7d552e41ed98ab8500
Rin <lvqq2000@gmail.com>
2026-04-28 19:04:33 +1000

    chore: adjust the user tips to be more descriptive

--- src/components/UsageInfo/GraphUsageInfo.tsx
+++ src/components/UsageInfo/GraphUsageInfo.tsx
@@ -4,16 +4,22 @@ export const GraphUsageInfo = () => {
 
     return (
         <>
-            <strong>Hierarchy View:</strong>
+            <strong>General Instructions:</strong>
             <br />
-            • Drag a goal in the left section to adjust the hierarchy
+            • Drag the divider between the two panels to the left or right to adjust the panel width
+            <br />
+            <br />
+            <strong>Hierarchy Panel:</strong>
+            <br />
+            • Drag a goal within the cluster to the left or right to adjust the hierarchical structure.
+            Drag to the right to deepen the hierarchy, and to the left to move it up a level
             <br />
             • Hover over a goal and click <BsPencilSquare /> to edit its name
             <br />
             • Hover over a goal and click <BsFillTrash3Fill /> to delete the goal
             <br />
             <br />
-            <strong>Model View:</strong>
+            <strong>Model Panel:</strong>
             <br />
             • Select and drag a goal to adjust its position on the graph
             <br />
